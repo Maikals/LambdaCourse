b2261900de8e642a946bf27ff2d0b0f3f4952fda
mcastanys <mcastanys@opentrends.net>
2016-12-23 14:52:51 +0100

    Removed unused imports

--- src/com/miquel/Lesson2/SolutionsLesson2.java
+++ src/com/miquel/Lesson2/SolutionsLesson2.java
@@ -8,7 +8,6 @@ package com.miquel.Lesson2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
